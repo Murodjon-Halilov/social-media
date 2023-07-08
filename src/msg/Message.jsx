70924f5b8c8506f498eebfa2b20ed28fea3b3e05
Murodjon Halilov <murodjonhalilov76@gmail.com>
2023-07-08 20:18:20 +0500

    for asilbek

--- src/msg/Message.jsx
+++ src/msg/Message.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MessageBox, AutherName, DateBox, TimeBox } from "./Message.style";
 import { MessageAndAuthContext } from "../contextFolder/messageContext";
 
@@ -7,27 +7,20 @@ const Message = () => {
 
   const msgData = usersData ? usersData.reverse() : "";
 
-  const date = new Date(1688825392154)
-
-  console.log(date)
-
-  return (
-    msgData ? msgData.map((userData) => (
+  return msgData
+    ? msgData.map((userData) => (
         <div key={userData._id}>
-          <DateBox>
-            {console.log(userData.time)}
-          </DateBox>
-        <MessageBox>
-          <AutherName>{userData.author}</AutherName>
-          {userData.message}
-          <TimeBox>
-          {userData.time.split(' ').splice(4, 1).join()}
-        </TimeBox>
-        </MessageBox>
+          <DateBox>{console.log(new Date(userData.time).toDateString())}</DateBox>
+          <MessageBox>
+            <AutherName>{userData.author}</AutherName>
+            {userData.message}
+            <TimeBox>
+              {new Date(userData.time).toString().split(' ').splice(4, 1).join()}
+            </TimeBox>
+          </MessageBox>
         </div>
       ))
-    : ""
-    );
+    : "";
 };
 
 export default Message;
